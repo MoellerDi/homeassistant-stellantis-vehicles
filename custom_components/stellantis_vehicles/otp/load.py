d7a0777a021856c993d520a690cd814c95f8d5c5
Dirk Moeller <MoellerDi@users.noreply.github.com>
2026-08-25 20:08:11 +0000

    Initial upload of the annotated OTP client code (provided by chmtc94) - not yet tested

--- custom_components/stellantis_vehicles/otp/load.py
+++ custom_components/stellantis_vehicles/otp/load.py
@@ -1,3 +1,16 @@
+"""Parsing of the InWebo "IW data" blob.
+
+The InWebo OTP mobile SDK stores its local state (account id, rotating
+keys, list of linked services, pending messages, etc.) as one big
+delimiter-separated string, historically produced by a Java client
+(hence the very compact/cryptic original field names such as ``iwK0``,
+``iwsrvn``...). This module decodes that blob into a Python object and
+knows how to merge in updates ("synchro") received from the server.
+
+Field names have been kept close to the protocol's own vocabulary
+(prefixed to indicate their group: device/account, keys, linked
+services, messages) but spelled out so their purpose is clear.
+"""
 import hashlib
 from locale import atoi
 from time import time
@@ -6,175 +19,223 @@ from Crypto.Cipher import AES
 
 from .tokenizer import Tokenizer
 
+# A "factory default" blob used to bootstrap a brand-new IWData object
+# before the very first real synchronisation with the server happens.
 DEFAULT_TOKEN = "0.2.11&&&&&&0&&0&&0&&9f13ba238fbabba08e85d93638e98ef5e48682a9d3e5bc325c3dd6fac8199a6ce09e9b4f373aa6a" \
                 "75a905c3d690f6e3335d1e8e5b748ecec3020a794149033f6ada6896db6d73b8d43b8365bbe15b9ac66f49d4e684a3628f1e" \
                 "9f3deda0c4e24aba771946e6085b92c5ad312477152acf8db01e6aea4b409d5ac1a05c2fd4e95&&0&&&&&&&&&&&&0&&0&&0&" \
                 "&0&&0&&0&&0&&&&&&&&0&&0&&0&&0&&0&&2.0.0&&http://m.inwebo.com/&&"
-DEFAULT_VERSION = "529"
+# Protocol/format version that DEFAULT_TOKEN was written for; controls
+# which optional fields load_iw_data_v1xx() expects to find.
+DEFAULT_FORMAT_VERSION = "529"
 
 
-def filter_load(string: str):
-    return string.replace("&amp;", "&")
+def unescape_ampersand(text: str):
+    """The server escapes literal '&' characters as '&amp;' so they don't
+    get confused with the '&&' field delimiter; undo that here."""
+    return text.replace("&amp;", "&")
 
 
 class IWData:
     # pylint: disable=invalid-name,too-many-branches,too-many-statements
-    def __init__(self, IW):
-        self.IW = IW
+    def __init__(self, otp_client):
+        """
+        :param otp_client: the owning Otp instance (needed for a couple of
+            flags such as ``is_mac_client`` that change how the blob is
+            structured).
+        """
+        self.otp_client = otp_client
         self.tokenizer = Tokenizer(DEFAULT_TOKEN)
-        self.tokenizer.nextToken()
-        self.load1xx(int(DEFAULT_VERSION), self.tokenizer)
+        self.tokenizer.next_token()  # discard the leading version token
+        self.load_iw_data_v1xx(int(DEFAULT_FORMAT_VERSION), self.tokenizer)
 
-    def load1xx(self, j, tokenizer):
-        self.iwid = tokenizer.nextToken()
-        self.iwalea = tokenizer.nextToken()
-        self.iwblocked = tokenizer.nextTokenI()
-        if j >= 519:
-            self.iwhasnopin = tokenizer.nextTokenI()
-        self.iwTsync = tokenizer.nextTokenI()
-        self.kfact = tokenizer.nextToken()
-        if self.IW.isMac:
-            self.iwconnected = tokenizer.nextTokenI()
-            self.iwserver = tokenizer.nextToken()
-        self.iwJ = tokenizer.nextToken()
-        self.iwK = tokenizer.nextToken()
-        self.iwK0 = tokenizer.nextToken()
-        self.iwK1 = tokenizer.nextToken()
-        self.iwTref = tokenizer.nextTokenI()
-        self.iwcancelpin = tokenizer.nextTokenI()
-        self.iwnboka = tokenizer.nextTokenI()
-        self.iwlastt1 = tokenizer.nextTokenI()
-        self.iwlastt2 = tokenizer.nextTokenI()
-        self.iwlastt3 = tokenizer.nextTokenI()
-        self.iwlastbp = tokenizer.nextTokenI()
-        self.iwstackrand = tokenizer.nextToken()
-        self.iwstack = tokenizer.nextToken()
-        self.iwH = tokenizer.nextToken()
-        nextTokenI = tokenizer.nextTokenI()
-        self.iwsrvn = nextTokenI
-        self.iwsrvid = [None] * (nextTokenI)
-        self.iwsrvname = [None] * (nextTokenI)
-        self.iwsrvlogo = [None] * (nextTokenI)
-        self.iwsrvurl = [None] * (nextTokenI)
-        self.iwsrvonlineotp = [None] * (nextTokenI)
-        if self.IW.isMac:
-            self.iwsrvconnected = [None] * (self.iwsrvn)
-        j2 = self.iwsrvn
-        self.iwsrvsecure = [None] * (j2)
-        self.iwsrvksc = [None] * (j2)
+    def load_iw_data_v1xx(self, format_version, tokenizer):
+        """Parse a "1.xx" format IW data blob token by token, in the exact
+        order the fields were written by the server/client."""
+        self.device_id = tokenizer.next_token()
+        self.device_alea = tokenizer.next_token()  # random value used to build the device serial
+        self.is_blocked = tokenizer.next_token_as_int()
+        if format_version >= 519:
+            self.has_no_pin = tokenizer.next_token_as_int()
+        self.last_sync_timestamp = tokenizer.next_token_as_int()
+        self.factory_key = tokenizer.next_token()
+        if self.otp_client.is_mac_client:
+            self.connected_timestamp = tokenizer.next_token_as_int()
+            self.server_url = tokenizer.next_token()
+        self.challenge_token = tokenizer.next_token()  # server field "J"
+        self.master_key = tokenizer.next_token()       # server field "K"
+        self.master_key_0 = tokenizer.next_token()      # server field "K0"
+        self.master_key_1 = tokenizer.next_token()      # server field "K1"
+        self.reference_timestamp = tokenizer.next_token_as_int()
+        self.cancel_pin_count = tokenizer.next_token_as_int()
+        self.ok_attempt_count = tokenizer.next_token_as_int()
+        self.last_attempt_time_1 = tokenizer.next_token_as_int()
+        self.last_attempt_time_2 = tokenizer.next_token_as_int()
+        self.last_attempt_time_3 = tokenizer.next_token_as_int()
+        self.last_bad_pin_time = tokenizer.next_token_as_int()
+        self.stack_random = tokenizer.next_token()
+        self.stack = tokenizer.next_token()
+        self.verification_hash = tokenizer.next_token()  # server field "H"
+
+        service_count = tokenizer.next_token_as_int()
+        self.service_count = service_count
+        self.service_ids = [None] * service_count
+        self.service_names = [None] * service_count
+        self.service_logos = [None] * service_count
+        self.service_urls = [None] * service_count
+        self.service_online_otp = [None] * service_count
+        if self.otp_client.is_mac_client:
+            self.service_connected = [None] * self.service_count
+        self.service_secure = [None] * service_count
+        self.service_ksc = [None] * service_count
+
         i = 0
-        while i < self.iwsrvn:
-            self.iwsrvid[i] = tokenizer.nextToken()
-            self.iwsrvname[i] = filter_load(tokenizer.nextToken())
-            self.iwsrvlogo[i] = filter_load(tokenizer.nextToken())
-            if self.IW.isMac:
-                self.iwsrvconnected[i] = tokenizer.nextTokenI()
-            if j > 515:
-                i2 = 1
-            elif j == 515:
-                i2 = 0
+        while i < self.service_count:
+            self.service_ids[i] = tokenizer.next_token()
+            self.service_names[i] = unescape_ampersand(tokenizer.next_token())
+            self.service_logos[i] = unescape_ampersand(tokenizer.next_token())
+            if self.otp_client.is_mac_client:
+                self.service_connected[i] = tokenizer.next_token_as_int()
+            if format_version > 515:
+                url_field_present = 1
+            elif format_version == 515:
+                url_field_present = 0
             else:
-                i2 = -1
-            if i2 < 0 or self.IW.isMac:
-                self.iwsrvurl[i] = ""
+                url_field_present = -1
+            if url_field_present < 0 or self.otp_client.is_mac_client:
+                self.service_urls[i] = ""
             else:
-                self.iwsrvurl[i] = filter_load(tokenizer.nextToken())
-            if j < 520 or self.IW.isMac:
-                self.iwsrvonlineotp[i] = 0
+                self.service_urls[i] = unescape_ampersand(tokenizer.next_token())
+            if format_version < 520 or self.otp_client.is_mac_client:
+                self.service_online_otp[i] = 0
             else:
-                self.iwsrvonlineotp[i] = tokenizer.nextTokenI()
-            self.iwsrvsecure[i] = tokenizer.nextToken()
-            if i2 < 0 or not self.IW.isMac:
-                self.iwsrvksc[i] = ""
+                self.service_online_otp[i] = tokenizer.next_token_as_int()
+            self.service_secure[i] = tokenizer.next_token()
+            if url_field_present < 0 or not self.otp_client.is_mac_client:
+                self.service_ksc[i] = ""
             else:
-                self.iwsrvksc[i] = tokenizer.nextToken()
+                self.service_ksc[i] = tokenizer.next_token()
             i += 1
-        nextTokenI2 = tokenizer.nextTokenI()
-        self.iwsecn = nextTokenI2
-        self.iwsecid = [None] * (nextTokenI2)
-        self.iwsecval = [None] * (nextTokenI2)
-        i3 = 0
-        while ((i3)) < self.iwsecn:
-            self.iwsecid[i3] = tokenizer.nextToken()
-            self.iwsecval[i3] = tokenizer.nextToken()
-            i3 += 1
-        self.iwmsgn = tokenizer.nextTokenI()
-        self.iwmsgtime = tokenizer.nextTokenI()
-        self.iwmsgid = ""
-        self.iwmsgtitle = ""
-        self.iwmsgcontent = ""
-        self.iwmsgack = ""
-        i4 = 0
-        while i4 < self.iwmsgn:
-            self.iwmsgid += tokenizer.nextToken()
-            self.iwmsgtitle += filter_load(tokenizer.nextToken())
-            self.iwmsgcontent += filter_load(tokenizer.nextToken())
-            self.iwmsgack += tokenizer.nextTokenI()
-            i4 += 1
-        self.iwmajorversion = tokenizer.nextTokenI()
-        self.iwnewversion = filter_load(tokenizer.nextToken())
-        self.iwnewversionurl = filter_load(tokenizer.nextToken())
-        self.mustupgrade = False
-        self.datatouch = 0
 
-    def synchro(self, ixml: dict, key):
-        aes_cipher = AES.new(bytes.fromhex(key), AES.MODE_ECB)
-        value = ixml.get("id")
+        secret_count = tokenizer.next_token_as_int()
+        self.secret_count = secret_count
+        self.secret_ids = [None] * secret_count
+        self.secret_values = [None] * secret_count
+        i = 0
+        while i < self.secret_count:
+            self.secret_ids[i] = tokenizer.next_token()
+            self.secret_values[i] = tokenizer.next_token()
+            i += 1
+
+        self.message_count = tokenizer.next_token_as_int()
+        self.message_time = tokenizer.next_token_as_int()
+        self.message_ids = ""
+        self.message_titles = ""
+        self.message_contents = ""
+        self.message_acks = ""
+        i = 0
+        while i < self.message_count:
+            self.message_ids += tokenizer.next_token()
+            self.message_titles += unescape_ampersand(tokenizer.next_token())
+            self.message_contents += unescape_ampersand(tokenizer.next_token())
+            self.message_acks += tokenizer.next_token_as_int()
+            i += 1
+
+        self.major_version = tokenizer.next_token_as_int()
+        self.new_version = unescape_ampersand(tokenizer.next_token())
+        self.new_version_url = unescape_ampersand(tokenizer.next_token())
+        self.must_upgrade = False
+        self.data_touched = 0
+
+    def apply_server_update(self, response_xml: dict, aes_key: str):
+        """Merge a partial update received from the server (a "synchro")
+        into the current state. Only the fields present in
+        ``response_xml`` are updated; everything else keeps its previous
+        value.
+
+        :param response_xml: dict-ified XML response from the server.
+        :param aes_key: hex-encoded AES key used to decrypt the fields
+            that come back encrypted (K0, K1, H).
+        """
+        aes_cipher = AES.new(bytes.fromhex(aes_key), AES.MODE_ECB)
+
+        value = response_xml.get("id")
         if value is not None and len(value) > 0:
-            self.iwid = value
-        value = ixml.get("server")
+            self.device_id = value
+
+        value = response_xml.get("server")
         if value is not None and len(value) > 0:
-            self.iwserver = value
-        value = ixml.get("K0")
+            self.server_url = value
+
+        value = response_xml.get("K0")
         if value is not None and len(value) > 0:
-            self.iwK0 = aes_cipher.decrypt(bytes.fromhex(value)).hex()
-        value = ixml.get("K1")
+            self.master_key_0 = aes_cipher.decrypt(bytes.fromhex(value)).hex()
+
+        value = response_xml.get("K1")
         if value is not None and len(value) > 0:
-            self.iwK1 = aes_cipher.decrypt(bytes.fromhex(value)).hex()
-        value = ixml.get("dK1")
+            self.master_key_1 = aes_cipher.decrypt(bytes.fromhex(value)).hex()
+
+        value = response_xml.get("dK1")
         if value is not None and len(value) > 0:
-            self.iwK1 = hashlib.sha256(("" + self.iwK1 + ";" + value).encode("utf-8")).hexdigest()[0:32]
-        value = ixml.get("J")
+            # K1 is rotated forward by hashing it together with the
+            # server-supplied delta "dK1".
+            self.master_key_1 = hashlib.sha256(
+                ("" + self.master_key_1 + ";" + value).encode("utf-8")
+            ).hexdigest()[0:32]
+
+        value = response_xml.get("J")
         if value is not None and len(value) > 0:
-            self.iwJ = value
-            self.iwTref = int(time())
-            self.IW.otpRetryService = -1
-        value = ixml.get("K")
+            self.challenge_token = value
+            self.reference_timestamp = int(time())
+            self.otp_client.otpRetryService = -1
+
+        value = response_xml.get("K")
         if value is not None and len(value) > 0:
-            self.iwK = value
-        value = ixml.get("H")
+            self.master_key = value
+
+        value = response_xml.get("H")
         if value is not None and len(value) > 0:
-            self.iwH = aes_cipher.decrypt(bytes.fromhex(value))
-        value = ixml.get("connected")
+            self.verification_hash = aes_cipher.decrypt(bytes.fromhex(value))
+
+        value = response_xml.get("connected")
         if value is not None and len(value) > 0:
-            self.iwconnected = atoi(value) + int(time())
-        value = ixml.get("s_n")
+            self.connected_timestamp = atoi(value) + int(time())
+
+        value = response_xml.get("s_n")
         if value is not None and len(value) > 0:
-            self.iwcancelpin = 0
-            self.iwnboka = 0
-            self.iwlastt1 = 0
-            self.iwlastt2 = 0
-            self.iwlastt3 = 0
-            self.iwlastbp = 0
-            self.iwstackrand = ""
-            self.iwstack = ""
-            self.iwTsync = ixml.get("Tsync")
-            self.iwsrvn = ixml.get("s_n")
-            self.iwsrvid = ixml.get("s_id")
-            self.iwsrvname = ixml.get("s_name")
-            self.iwsrvlogo = ixml.get("s_icon")
-            self.iwsrvconnected = ixml.get("s_connected")
-            self.iwsrvksc = ixml.get("s_ksc")
-            self.iwsrvsecure = ixml.get("s_secure")
-            self.iwsrvurl = ixml.get("s_url")
-            self.iwsrvonlineotp = ixml.get("s_onlineotp")
-            self.IW.synchroJustDone = 1
-        value = ixml.get("m_n")
+            # A full "linked services" list was sent: reset the
+            # per-attempt counters and replace the service list wholesale.
+            self.cancel_pin_count = 0
+            self.ok_attempt_count = 0
+            self.last_attempt_time_1 = 0
+            self.last_attempt_time_2 = 0
+            self.last_attempt_time_3 = 0
+            self.last_bad_pin_time = 0
+            self.stack_random = ""
+            self.stack = ""
+            self.last_sync_timestamp = response_xml.get("Tsync")
+            self.service_count = response_xml.get("s_n")
+            self.service_ids = response_xml.get("s_id")
+            self.service_names = response_xml.get("s_name")
+            self.service_logos = response_xml.get("s_icon")
+            self.service_connected = response_xml.get("s_connected")
+            self.service_ksc = response_xml.get("s_ksc")
+            self.service_secure = response_xml.get("s_secure")
+            self.service_urls = response_xml.get("s_url")
+            self.service_online_otp = response_xml.get("s_onlineotp")
+            self.otp_client.synchroJustDone = 1
+
+        value = response_xml.get("m_n")
         if value is not None and len(value) > 0:
-            self.iwmsgtime = int(time())
-            self.iwmsgn = ixml.get("m_n")
-            self.iwmsgid = ixml.get("m_id")
-            self.iwmsgtitle = ixml.get("m_title")
-            self.iwmsgcontent = ixml.get("m_content")
-            self.iwmsgack = ixml.get("m_ack")
-        self.datatouch = 1
+            self.message_time = int(time())
+            self.message_count = response_xml.get("m_n")
+            self.message_ids = response_xml.get("m_id")
+            self.message_titles = response_xml.get("m_title")
+            self.message_contents = response_xml.get("m_content")
+            self.message_acks = response_xml.get("m_ack")
+
+        self.data_touched = 1
+
+    # Backward-compatible alias: the server-facing code originally called
+    # this method "synchro".
+    synchro = apply_server_update
--- custom_components/stellantis_vehicles/otp/oaep.py
+++ custom_components/stellantis_vehicles/otp/oaep.py
@@ -1,3 +1,19 @@
+"""Custom PKCS#1 OAEP decryption.
+
+This is a thin variant of pycryptodome's ``PKCS1OAEP_Cipher``: it
+re-implements ``decrypt`` so it deliberately skips the strict "leading
+byte must be 0" check that the stock implementation performs. That
+check is normally there as an extra correctness guard, but checking it
+separately (before the rest of the padding is validated) can leak
+timing information that an attacker can use to decrypt ciphertexts
+without knowing the private key (a "Manger's attack"). Folding that
+byte into the same constant-time comparison as everything else, like
+this implementation does, avoids that.
+
+This is needed here because InWebo's server-side RSA/OAEP
+implementation does not always produce a leading zero byte, so the
+stock pycryptodome check would reject otherwise-valid responses.
+"""
 import Crypto
 from Crypto import Random
 from Crypto.Cipher.PKCS1_OAEP import PKCS1OAEP_Cipher
@@ -6,7 +22,7 @@ from Crypto.Util.py3compat import bord
 from Crypto.Util.strxor import strxor
 
 
-class MyOAEP(PKCS1OAEP_Cipher):
+class NonStrictOAEPCipher(PKCS1OAEP_Cipher):
     # pylint: disable=too-many-locals,invalid-name
     def decrypt(self, ciphertext):
         """Decrypt a message with PKCS#1 OAEP.
@@ -26,61 +42,77 @@ class MyOAEP(PKCS1OAEP_Cipher):
             to decrypt using a public key).
         """
 
-        # See 7.1.2 in RFC3447
-        mod_bits = Crypto.Util.number.size(self._key.n)
-        k = ceil_div(mod_bits, 8)  # Convert from bits to bytes
-        h_len = self._hashObj.digest_size
+        # See section 7.1.2 of RFC3447 for the numbered steps below.
+        modulus_bits = Crypto.Util.number.size(self._key.n)
+        modulus_len_bytes = ceil_div(modulus_bits, 8)  # convert from bits to bytes
+        hash_len = self._hashObj.digest_size
 
-        # Step 1b and 1c
-        if len(ciphertext) != k:
+        # Step 1b and 1c: the ciphertext must be exactly one RSA block long.
+        if len(ciphertext) != modulus_len_bytes:
             raise ValueError("Ciphertext with incorrect length.")
-        # Step 2a (O2SIP)
-        ct_int = bytes_to_long(ciphertext)
-        # Step 2b (RSADP)
-        # m_int = self._key._decrypt(ct_int)
-        m_int = pow(ct_int, self._key.e, self._key.n)
 
-        # Complete step 2c (I2OSP)
-        em = long_to_bytes(m_int, k)
-        # Step 3a
-        l_hash = self._hashObj.new(self._label).digest()
-        # Step 3b
-        y = em[0]
-        # y must be 0, but we MUST NOT check it here in order not to
-        # allow attacks like Manger's (http://dl.acm.org/citation.cfm?id=704143)
-        masked_seed = em[1:h_len + 1]
-        masked_db = em[h_len + 1:]
-        # Step 3c
-        seed_mask = self._mgf(masked_db, h_len)
-        # Step 3d
+        # Step 2a (OS2IP): ciphertext bytes -> integer.
+        ciphertext_int = bytes_to_long(ciphertext)
+        # Step 2b (RSADP): raw RSA decryption (modular exponentiation).
+        # plaintext_int = self._key._decrypt(ciphertext_int)
+        plaintext_int = pow(ciphertext_int, self._key.e, self._key.n)
+
+        # Step 2c (I2OSP): integer -> fixed-length "encoded message" bytes.
+        encoded_message = long_to_bytes(plaintext_int, modulus_len_bytes)
+
+        # Step 3a: hash of the (empty, here) OAEP label.
+        label_hash = self._hashObj.new(self._label).digest()
+
+        # Step 3b: split the encoded message into its three parts.
+        leading_byte = encoded_message[0]
+        # This leading byte is supposed to always be 0, but it is
+        # intentionally NOT checked in isolation here, to avoid timing
+        # side-channel attacks such as Manger's
+        # (http://dl.acm.org/citation.cfm?id=704143). It is instead
+        # folded into the constant-time validity check performed below.
+        masked_seed = encoded_message[1:hash_len + 1]
+        masked_data_block = encoded_message[hash_len + 1:]
+
+        # Step 3c: recompute the mask that was XORed onto the seed.
+        seed_mask = self._mgf(masked_data_block, hash_len)
+        # Step 3d: unmask the seed.
         seed = strxor(masked_seed, seed_mask)
-        # Step 3e
-        db_mask = self._mgf(seed, k - h_len - 1)
-        # Step 3f
-        db = strxor(masked_db, db_mask)
-        # Step 3g
-        one_pos = db[h_len:].find(b'\x01')
-        l_hash1 = db[:h_len]
-        invalid = bord(y) | int(one_pos < 0)
-        hash_compare = strxor(l_hash1, l_hash)
-        for x in hash_compare:
-            invalid |= bord(x)
-        for x in db[h_len:one_pos]:
-            invalid |= bord(x)
-        if invalid != 0:
+        # Step 3e: recompute the mask that was XORed onto the data block.
+        data_block_mask = self._mgf(seed, modulus_len_bytes - hash_len - 1)
+        # Step 3f: unmask the data block.
+        data_block = strxor(masked_data_block, data_block_mask)
+
+        # Step 3g: the data block is [label_hash | zero padding | 0x01 | message].
+        # Locate the 0x01 separator, then compare everything in
+        # constant time (accumulating into a single "invalid" flag
+        # rather than branching/returning early) so that no timing
+        # information about *where* validation failed is leaked.
+        separator_pos = data_block[hash_len:].find(b'\x01')
+        label_hash_from_block = data_block[:hash_len]
+        is_invalid = bord(leading_byte) | int(separator_pos < 0)
+        label_hash_diff = strxor(label_hash_from_block, label_hash)
+        for byte in label_hash_diff:
+            is_invalid |= bord(byte)
+        for byte in data_block[hash_len:separator_pos]:
+            is_invalid |= bord(byte)
+        if is_invalid != 0:
             raise ValueError("Incorrect decryption.")
-        # Step 4
-        return db[h_len + one_pos + 1:]
+
+        # Step 4: everything after the 0x01 separator is the plaintext.
+        return data_block[hash_len + separator_pos + 1:]
 
 
 def new(key, hash_algo=None, mgfunc=None, label=b'', rand_func=None):
+    """Build a NonStrictOAEPCipher for ``key`` (construction mirrors
+    ``Crypto.Cipher.PKCS1_OAEP.new``)."""
     if rand_func is None:
         rand_func = Random.get_random_bytes
-    return MyOAEP(key, hash_algo, mgfunc, label, rand_func)
+    return NonStrictOAEPCipher(key, hash_algo, mgfunc, label, rand_func)
 
 
-# for testing
-def notrandom(x):
-    if x == 32:
+# Fixed "random" seed used only by unit tests, so OAEP encryption
+# output is reproducible instead of different on every run.
+def fixed_test_seed(length):
+    if length == 32:
         return b'\xf56\xccL`\x8a\x97l\nX0\xf4\x11\x9a\x0e\xce\x99K^\xe6\xcbU\xf3W+It"\xf5\x84\x1d\xe6'
     return None
